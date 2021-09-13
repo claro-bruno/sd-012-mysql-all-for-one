08e6fa2f326d80d92029d25f2fcbb5dd47f8124f
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-09-13 19:54:32 -0300

    Projeto All for One completo

---
+++ desafio15.sql
@@ -0,0 +1,4 @@
+SELECT 
+    HOUR(submitted_date) as submitted_hour
+FROM
+    northwind.purchase_orders;
---
+++ desafio18.sql
@@ -0,0 +1,7 @@
+SELECT 
+    *
+FROM
+    northwind.purchase_orders
+WHERE
+    supplier_id = 3 AND status_id = 2;
+
