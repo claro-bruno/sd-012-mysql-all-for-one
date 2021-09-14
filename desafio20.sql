d4e1084d0a0f3665963812545e20977a8a24db03
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-09-14 18:08:18 -0300

    Corrigindo os erros de linter 6

--- desafio20.sql
+++ desafio20.sql
@@ -1,2 +1,2 @@
 INSERT INTO northwind.order_details(order_id, product_id, quantity, unit_price, status_id, inventory_id)
-	VALUES (69, 80, 15.0000, 15.0000, 2, 129);
+VALUES (69, 80, 15.0000, 15.0000, 2, 129);
