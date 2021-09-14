9549d01dade371f396e8a3e5b9943c8e571608b1
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-09-14 15:15:01 -0300

    Requisito 19 - Selecionar com base nos ids a quantidade

---
+++ desafio19.sql
@@ -0,0 +1,2 @@
+-- 19 - Mostre a quantidade de pedidos que foram feitos na tabela orders pelo employee_id igual a 5 ou 6, e que foram enviados através do método(coluna) shipper_id igual a 2.
+SELECT COUNT(*) AS 'orders_count' FROM orders WHERE employee_id IN (5, 6) AND shipper_id = 2;
