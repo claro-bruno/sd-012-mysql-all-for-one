ae732e358f7c78626335bfee23708e707f169be4
Eric Faria <ericdeofaria@gmail.com>
2021-09-14 17:31:00 -0300

    Correções de estilo

--- desafio11.sql
+++ desafio11.sql
@@ -1,3 +1,2 @@
-SELECT notes FROM northwind.purchase_orders WHERE notes IS NOT NULL AND RIGHT(notes, 2) >= 30 AND RIGHT(notes, 2) <= 39 ;
--- fonte sobre RIGHT(): https://elias.praciano.com/2015/04/como-decompor-ou-combinar-strings-no-mysql/
+SELECT notes FROM northwind.purchase_orders WHERE notes IS NOT NULL AND RIGHT(notes, 2) >= 30 AND RIGHT(notes, 2) <= 39 ; -- fonte sobre RIGHT(): https://elias.praciano.com/2015/04/como-decompor-ou-combinar-strings-no-mysql/
  
