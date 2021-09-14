1ed0d03e8864cd1148c4cd941ecbf0b2d6c45a62
matheustkaczyk <matheus111904@hotmail.com>
2021-09-14 12:42:35 -0300

    Desafio 20

--- desafio19.sql
+++ desafio19.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(*) FROM northwind.orders WHERE employee_id IN (5, 6) AND shipper_id = 2;
+SELECT COUNT(*) AS orders_count FROM northwind.orders WHERE employee_id IN (5, 6) AND shipper_id = 2;
