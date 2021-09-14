353a8e650c03bd93c4b15bf003c6e4e68d48c95d
Daniel Ceci <danielceci96@gmail.com>
2021-09-14 16:56:20 -0300

    corrigindo lint requisito 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,2 +1,2 @@
 SELECT id FROM northwind.products;
--- chave primária de products é o id. Chave primária é um identificador único da tabela, aquela que não terá valor repetido
+-- chave primária de products é o id. Chave primária é um identificador único da tabela, aquela que não terá valor repetido.
