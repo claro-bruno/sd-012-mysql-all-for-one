0aaeaa640c5182b1e22026dbd5b1da5be9d487b7
ThalitaC <thalitaceci@gmail.com>
2021-09-14 20:12:53 -0300

    fix: desafio 20

--- desafio20.sql
+++ desafio20.sql
@@ -1,2 +1,2 @@
 INSERT INTO northwind.order_details(order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
-VALUES (69, 80, 150000, 150000, 0, 2, NULL, NULL, 129);
+VALUES (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
