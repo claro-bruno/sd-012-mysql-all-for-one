c52644174410b833d46fc84509f2a2ee270c767e
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-09-14 14:19:43 -0300

    Iniciando Projeto

---
+++ desafio10.sql
@@ -0,0 +1,8 @@
+SELECT 
+  * 
+FROM 
+  northwind.purchase_orders
+WHERE
+  created_by >= 3
+ORDER BY
+  created_by DESC, id;
---
+++ desafio11.sql
@@ -0,0 +1,6 @@
+SELECT
+	notes
+FROM 
+	northwind.purchase_orders
+WHERE 
+	SUBSTRING(notes, 36, 37) BETWEEN 30 AND 39;
