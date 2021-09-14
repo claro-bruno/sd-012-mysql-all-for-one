170ba7315d9b49d76c64822f5bd969071c5fbd30
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-09-14 14:57:18 -0300

    Requisito 12 - Itens no dia solicitado

---
+++ desafio12.sql
@@ -0,0 +1,2 @@
+-- 12 - Mostre as submitted_date de purchase_orders em que a submitted_date é do dia 26 de abril de 2006.
+SELECT submitted_date FROM purchase_orders WHERE submitted_date BETWEEN '2006-04-26 00:00:00' AND '2006-04-26 23:59:59';
