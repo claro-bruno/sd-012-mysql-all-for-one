416df0a7a90bb099ebdbe1577a52065b91b2ede1
Fabio Ryudi <ryu_fabio@hotmail.com>
2021-09-14 18:30:23 -0300

    adiciona requisitos de filtragem de dados

---
+++ desafio11.sql
@@ -0,0 +1,1 @@
+SELECT notes FROM northwind.purchase_orders WHERE notes LIKE '%#30' OR notes LIKE '%#31' OR notes LIKE '%#32' OR notes LIKE '%#33' OR notes LIKE '%#34' OR notes LIKE '%#35' OR notes LIKE '%#36' OR notes LIKE '%#37' OR notes LIKE '%#38' OR notes LIKE '%#39';
