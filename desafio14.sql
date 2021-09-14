62bfe6efd2633af5979b74caefd4e3941bbb54a0
André Moreno <deco616@hotmail.com>
2021-09-14 17:03:01 -0300

    Ajusta Desafio 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,3 +1,7 @@
-SELECT supplier_id FROM northwind.purchase_orders
-WHERE supplier_id >= 1 AND supplier_id <= 3;
+SELECT 
+    supplier_id
+FROM
+    northwind.purchase_orders
+WHERE
+    supplier_id >= 1 AND supplier_id <= 3;
  
