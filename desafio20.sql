de3eefc8a09fd779d5da5804923b61dfbb8d6b08
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-09-14 15:09:57 -0300

    requisitos 20 e 21 fixx

--- desafio20.sql
+++ desafio20.sql
@@ -1,2 +1,2 @@
-  INSERT INTO nortwind.order_details(order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
+  INSERT INTO northwind.order_details (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
     VALUES(69, 80, 15.000, 15.000, 0, 2, NULL, NULL, 129);
