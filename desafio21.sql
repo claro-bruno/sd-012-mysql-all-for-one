f17ca3dbdddaa72efd5279852331b64e4faa1d24
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-09-14 17:34:59 -0300

    adiciona desafio 21

---
+++ desafio21.sql
@@ -0,0 +1,4 @@
+insert into northwind.order_details (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
+values
+	(69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129),
+	(69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
