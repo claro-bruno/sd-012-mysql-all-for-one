cf3d55101597c877aaa9bfe8c91934ca6d1474cc
Caio Morato <morato_dev@protonmail.com>
2021-09-14 14:52:58 -0300

    ajuste

--- desafio16.sql
+++ desafio16.sql
@@ -1,2 +1,3 @@
 SELECT submitted_date FROM northwind.purchase_orders
-WHERE submitted_date BETWEEN '2006-01-14' AND '2006-03-31';
+WHERE submitted_date BETWEEN ('2006/01/26 00:00:00') 
+AND ('2006/03/31 23:59:59');
