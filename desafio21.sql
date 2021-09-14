917f87e2f2a088b7f665471325dbc79f619f343f
José Carlos <kalarruda@hotmail.com>
2021-09-14 17:15:33 -0300

    desafios 20 e 21 sem SELECT

--- desafio21.sql
+++ desafio21.sql
@@ -1,7 +1,4 @@
-SELECT * FROM northwind.order_details WHERE order_id=69;
-
-INSERT INTO northwind.order_details
-(order_id, product_id, quantity, unit_price, discount, status_id, status_id, date_allocated, purchase_order_id, inventory_id)
+INSERT INTO northwind.order_details (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
 VALUES
 (69, 80, 15.000, 15.000, 0, 2, NULL, NULL, 129),
 (69, 80, 15.000, 15.000, 0, 2, NULL, NULL, 129);
