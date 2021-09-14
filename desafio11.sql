ed2391063a349bf743742db86cd34c3cc55628bd
Eric Faria <ericdeofaria@gmail.com>
2021-09-14 17:38:18 -0300

    Correções de estilo

--- desafio11.sql
+++ desafio11.sql
@@ -5,6 +5,4 @@ FROM
 WHERE
   notes IS NOT NULL
   AND RIGHT(notes, 2) >= 30
-  AND RIGHT(notes, 2) <= 39;-- fonte sobre RIGHT(): https://elias.praciano.com/2015/04/como-decompor-ou-combinar-strings-no-mysql/
-
- 
+  AND RIGHT(notes, 2) <= 39;
