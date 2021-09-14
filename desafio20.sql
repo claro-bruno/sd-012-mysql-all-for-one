d208cc6df29d4f15182aed9000523b53ca038252
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-09-14 15:30:36 -0300

    Requisito 21 - Adicionar os mesmos dados do requisito anterior mas duas vezes em um insert

--- desafio20.sql
+++ desafio20.sql
@@ -2,4 +2,4 @@
 unit_price: 15.0000, discount: 0, status_id: 2, date_allocated: NULL, purchase_order_id: NULL e 
 inventory_id: 129. */
 INSERT INTO order_details (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
-VALUE (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
+VALUES (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
