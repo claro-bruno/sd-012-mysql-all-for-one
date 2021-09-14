1603ed386915f1d098c3e79b8f116977ae1a1b6e
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-09-14 19:53:16 -0300

    Corrigindo Desafio 13

--- desafio13.sql
+++ desafio13.sql
@@ -1,3 +1,3 @@
 -- 13 - Mostre o `supplier_id` das `purchase_orders` em que o `supplier_id` seja 1 ou 3.
 
-SELECT supplier_id FROM northwind.purchase_orders WHERE suplier_id IN(1, 3);
+SELECT supplier_id FROM purchase_orders WHERE suplier_id = 1 OR suplier_id = 3;
