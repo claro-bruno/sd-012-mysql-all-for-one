ee694169a8bb1cccceab39e0e796edd40d53738a
Erick Silva <erick.silva6653@gmail.com>
2021-09-14 13:04:40 -0300

    finalizando projeto

---
+++ desafio12.sql
@@ -0,0 +1,6 @@
+SELECT 
+    submitted_date
+FROM
+    northwind.purchase_orders
+WHERE
+    submitted_date like '2006-04-26%';
---
+++ desafio8.sql
@@ -0,0 +1,2 @@
+SELECT 
+    5 + 6 as A, 'de' as Trybe, 2 + 8 as 'eh';
