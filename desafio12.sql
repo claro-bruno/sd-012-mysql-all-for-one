560d3eb34996415aad27ce1565c3a3367e826d28
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-09-14 15:26:05 -0300

    codeclimate Unexpected tabs found

--- desafio12.sql
+++ desafio12.sql
@@ -2,5 +2,4 @@ SELECT
     submitted_date
 FROM
     northwind.purchase_orders
-WHERE submitted_date
-	LIKE '2006-04-26%';
+WHERE submitted_date LIKE '2006-04-26%';
--- desafio17.sql
+++ desafio17.sql
@@ -2,5 +2,4 @@ SELECT
     id, supplier_id
 FROM
     northwind.purchase_orders
-WHERE supplier_id
-	IN (1, 3, 5, 7);
+WHERE supplier_id IN (1, 3, 5, 7);
