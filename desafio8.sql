5b9e758990a0e3bf69ab57c69b3a41ed1e580964
Samuel Constantino <samuelicapuidados@gmail.com>
2021-09-14 15:22:05 -0300

    implementa Requisitos Iniciais 2 - 8

---
+++ desafio8.sql
@@ -0,0 +1,16 @@
+/*
+Faça uma consulta que retorne três colunas, respectivamente, 
+com os nomes 'A', 'Trybe' e 'eh', 
+e com valores referentes a soma de 
+'5 + 6', a string 'de', a soma de '2 + 8'.
+Observações técnicas
+
+Na primeira coluna, exiba a soma de 5 + 6 (essa soma deve ser realizada pelo SQL).
+Na segunda coluna deve haver a palavra "de".
+E por fim, na terceira coluna, exiba a soma de 2 + 8 (essa soma deve ser realizada pelo SQL).
+A primeira coluna deve se chamar "A", a segunda coluna deve se chamar "Trybe" 
+e a terceira coluna deve se chamar "eh".
+Não use colunas pre-existentes, apenas o que for criado na hora.
+*/
+
+SELECT 5 + 6 AS A, 'de' AS Trybe, 2 + 8 AS eh;
