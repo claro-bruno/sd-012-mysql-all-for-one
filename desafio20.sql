01d169d0d4da2d18cf9071c8c38bcb8e943ceba4
José Carlos <kalarruda@hotmail.com>
2021-09-14 16:15:52 -0300

    desafio 20

---
+++ desafio20.sql
@@ -0,0 +1,5 @@
+SELECT * FROM northwind.order_details WHERE order_id=69;
+
+INSERT INTO northwind.order_details (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
+VALUES 
+	(69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
