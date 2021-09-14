ea1644e171246be65f8689bc97df189b9c19da9a
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-09-14 18:50:40 -0300

    Retira os backticks dos arquivos dos desafios 20 e 21

--- desafio20.sql
+++ desafio20.sql
@@ -1,4 +1,4 @@
-INSERT INTO `northwind`.`order_details`
+INSERT INTO northwind.order_details
 (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
 VALUES
 (69, 80, 15000, 15000, 0, 2, NULL, NULL, 129);
