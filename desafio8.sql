d2ed65d3c6e9eaa2bbf663fe58f75ce38bced61f
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-09-14 14:49:39 -0300

    Requisito 10 - Ordenar por create by em ordem decrescente que seja maior que 3 e depois por id em ordem crescente

--- desafio8.sql
+++ desafio8.sql
@@ -1,2 +1,2 @@
--- 8 - Faça uma consulta que retorne três colunas, respectivamente, com os nomes 'A', 'Trybe' e 'de', e com valores referentes a soma de '5 + 6', a string 'de', a soma de '2 + 8'.
+-- 8 - Faça uma consulta que retorne três colunas, respectivamente, com os nomes 'A', 'Trybe' e 'eh', e com valores referentes a soma de '5 + 6', a string 'de', a soma de '2 + 8'.
 SELECT (5 + 6) AS 'A', 'de' AS 'Trybe', (2 + 8) AS 'eh';
