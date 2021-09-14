da950b1516fa0ec3811140eb5cc4224865b97d12
Samuel Constantino <samuelicapuidados@gmail.com>
2021-09-14 18:50:46 -0300

    fix code climate

--- desafio11.sql
+++ desafio11.sql
@@ -8,5 +8,5 @@ SELECT
     notes
 FROM
     northwind.purchase_orders
-where
-	notes LIKE 'Purchase generated based on Order #3_';
+WHERE
+    notes LIKE 'Purchase generated based on Order #3_';
--- desafio17.sql
+++ desafio17.sql
@@ -8,4 +8,6 @@ SELECT
 FROM
     purchase_orders
 WHERE
-	supplier_id = 1 OR supplier_id = 3 OR supplier_id = 5 OR supplier_id = 7;
+    supplier_id = 1 OR supplier_id = 3
+        OR supplier_id = 5
+        OR supplier_id = 7;
