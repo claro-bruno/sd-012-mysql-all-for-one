435e699fcd211e215e5ca9ebe55a18bedbbc8e99
CamilaDamasio <camiladamasioef@hotmail.com>
2021-09-14 15:10:04 -0300

    Verifica requisitos 21 a 24

--- desafio21.sql
+++ desafio21.sql
@@ -2,7 +2,7 @@
 -- Observações técnicas
 -- Esses dados são novamente order_id: 69, product_id: 80, quantity: 15.0000, unit_price: 15.0000, discount: 0, status_id: 2, date_allocated: NULL, purchase_order_id: NULL e inventory_id: 129.
 -- O ìd deve ser incrementado automaticamente.
-SELECT * FROM order_details;
+-- SELECT * FROM order_details;
 INSERT INTO `northwind`.`order_details`
     (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
 VALUES
--- desafio23.sql
+++ desafio23.sql
@@ -1,7 +1,7 @@
 -- Atualize os dados da coluna discount da tabela order_details para 30, onde o valor na coluna unit_price seja menor que 10.0000.
 -- Observações técnicas
 -- Não é necessário utilizar o SAFE UPDATE em sua query.
-SELECT * FROM order_details;
+-- SELECT * FROM order_details;
 UPDATE order_details
 SET discount = 30
 WHERE unit_price < 10.0000;
