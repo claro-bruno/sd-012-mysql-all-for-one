dd45beb9bfd40004839860939dbde5fca3f68205
Renan Ferreira <renansantiago2013@hotmail.com>
2021-09-14 17:46:05 -0300

    Resolve os requisitos de 20 a 27(ajustes no lint)

--- desafio21.sql
+++ desafio21.sql
@@ -1,4 +1,4 @@
 INSERT IGNORE INTO northwind.order_details (order_id, product_id, quantity, unit_price, discount, status_id, inventory_id)
 VALUES 
-	(69, 80, 15.000, 15.000, 0, 2, 129),
-  (69, 80, 15.000, 15.000, 0, 2, 129);
+(69, 80, 15.000, 15.000, 0, 2, 129),
+(69, 80, 15.000, 15.000, 0, 2, 129);
