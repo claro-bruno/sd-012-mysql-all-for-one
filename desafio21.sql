dd8a8cd56aa0f711ec9414ec820ea942bf992ae0
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-09-14 20:38:12 -0300

    Desafios de manipulação de tabelas

---
+++ desafio21.sql
@@ -0,0 +1,5 @@
+INSERT INTO northwind.order_details
+	(order_id, product_id, quantity, unit_price, discount, status_id, inventory_id)
+values
+	(69, 80, 15.000, 15.000, 0, 2, 129),
+    (69, 80, 15.000, 15.000, 0, 2, 129);
