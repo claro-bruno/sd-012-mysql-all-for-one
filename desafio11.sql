25d85fb6cce100e1bf5cc2158b9288ad0f3bfa4b
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-09-14 19:06:27 -0300

    Corrigindo requisito 11 apoa a mentoria

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,4 @@
 -- 11 - Exiba os dados da coluna `notes` da tabela `purchase_orders` em que seu valor 
 -- de `Purchase generated based on Order` é maior ou igual a 30 e menor ou igual a 39.
 
-SELECT 
-    *
-FROM
-    purchase_orders
-WHERE
-    notes LIKE '%3_'; 
+SELECT notes FROM purchase_orders WHERE notes LIKE '%3_';
