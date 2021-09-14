b950e124d2e7c17823ec505bafe05e4cf83807b8
Ramon Paschoal <rpaschoal90@gmail.com>
2021-09-14 16:33:10 -0300

    Requisitos 10 ao 19

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+SELECT notes FROM northwind.purchase_orders
+	WHERE notes IN
+    ('Purchase generated based on Order #30',
+    'Purchase generated based on Order #33',
+    'Purchase generated based on Order #36',
+    'Purchase generated based on Order #38',
+    'Purchase generated based on Order #39');
