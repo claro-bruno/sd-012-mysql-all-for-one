b6cdd556e74bd96e17a85ceff1ccf7c1ff940a7e
CamilaDamasio <camiladamasioef@hotmail.com>
2021-09-14 14:59:29 -0300

    Corrige requisitos 8 e 19

--- desafio19.sql
+++ desafio19.sql
@@ -2,4 +2,4 @@
 -- Observações técnicas
 -- Chame a coluna de orders_count.
 SELECT * FROM orders;
-SELECT COUNT(*) AS orders_count FROM orders WHERE employee_id = 5 OR employee_id = 6 AND shipper_id = 2;
+SELECT COUNT(*) AS orders_count FROM orders WHERE employee_id IN (5, 6) AND shipper_id = 2;
--- desafio8.sql
+++ desafio8.sql
@@ -5,4 +5,4 @@
 -- E por fim, na terceira coluna, exiba a soma de 2 + 8 (essa soma deve ser realizada pelo SQL).
 -- A primeira coluna deve se chamar "A", a segunda coluna deve se chamar "Trybe" e a terceira coluna deve se chamar "eh".
 -- Não use colunas pre-existentes, apenas o que for criado na hora.
-SELECT 5+6 AS 'A', 'de' AS 'TRYBE', 2+8 AS 'eh';
+SELECT 5+6 AS A, 'de' AS Trybe, 2+8 AS eh;
