477e05905da57839bf7d12e75326b748e51c7557
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-09-14 17:57:19 -0300

    Resolve a identaçao

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,4 @@
 -- 5 - Monte uma query que exiba os dados da tabela
 -- `products` a partir do quarto registro até o décimo terceiro.
 
- SELECT * FROM products limit 10 OFFSET 3;
- 
+SELECT * FROM products limit 10 OFFSET 3;
