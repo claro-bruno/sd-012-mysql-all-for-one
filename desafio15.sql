9ff0c2b4ceef6c56402b5a8e386a2d87e7905dbc
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-09-14 20:37:27 -0300

    Corrigindo o nome da coluna desafio 16

--- desafio15.sql
+++ desafio15.sql
@@ -1,4 +1,7 @@
 -- 15 - Mostre somente as horas (sem os minutos e os segundos) da coluna 
 -- `submitted_date` de todos registros da tabela `purchase_orders`.
 
-SELECT HOUR(submitted_date) AS 'hours' FROM northwind.purchase_orders;
+SELECT 
+    HOUR(submitted_date) AS 'submitted_hour'
+FROM
+    northwind.purchase_orders;
