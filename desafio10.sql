d7c6d83290ac6b7ea9ce5e7dbea834d4ced892bd
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-09-14 14:52:51 -0300

    add desafios 11 ao 13

--- desafio10.sql
+++ desafio10.sql
@@ -1,3 +1,3 @@
-SELECT * FROM purchase_orders
+SELECT * FROM northwind.purchase_orders
 WHERE created_by >= 3
 ORDER BY created_by DESC, id;
--- desafio9.sql
+++ desafio9.sql
@@ -1,2 +1,2 @@
-SELECT notes FROM purchase_orders
+SELECT notes FROM northwind.purchase_orders
 WHERE notes IS NOT NULL;
