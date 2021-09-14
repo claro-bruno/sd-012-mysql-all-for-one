014a02984eec08be6e26eeb1c1342b7d5f6b04b4
Flavio_Cordeiro <flavio_ribeirorc@hotmail.com>
2021-09-14 18:05:57 -0300

    Bugs 20 e 21

--- desafio21.sql
+++ desafio21.sql
@@ -1,4 +1,4 @@
-INSERT INTO northwind.order_details (order_id, product_id, quantity, product_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
+INSERT INTO northwind.order_details (order_id, product_id, quantity, unit_price, discount, status_id, inventory_id)
 VALUES
 (69, 80, 15.0000, 15.0000, 0, 2, 129),
 (69, 80, 15.0000, 15.0000, 0, 2, 129);
