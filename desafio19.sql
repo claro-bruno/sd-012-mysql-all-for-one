31b00940f4c5df7abad2d63510b7b3cf60cb7820
Gabriel Takaki <gabitj@icloud.com>
2021-09-14 17:18:03 -0300

    Corrige requisitos 19 ao 21

--- desafio19.sql
+++ desafio19.sql
@@ -1,2 +1,2 @@
-SELECT COUNT(*) FROM northwind.orders
+SELECT COUNT(*) AS orders_count FROM northwind.orders
 WHERE employee_id IN (5, 6) AND shipper_id = 2;
--- desafio20.sql
+++ desafio20.sql
@@ -1,3 +1,2 @@
-INSERT INTO northwind.order_details(order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
-VALUES(69, 80, 15.000, 15.000, 0, 2, NULL, NULL, 129);
-SELECT * FROM northwind.order_details;
+INSERT INTO `northwind`.`order_details`(order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
+VALUES(69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
