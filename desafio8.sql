1f02d71dc3eaf2276687b485d18c76775a726115
Renata Nunes <renatanunes231@yahoo.com.br>
2021-09-14 17:43:49 -0300

    Add ultima linha vazia aos Desafios Iniciais

--- desafio8.sql
+++ desafio8.sql
@@ -1,4 +1,4 @@
 -- Faça uma consulta que retorne três colunas, respectivamente, com os nomes 'A', 'Trybe' e 'eh',
 -- e com valores referentes a soma de '5 + 6', a string 'de', a soma de '2 + 8'.
 
-select 5 + 6 as 'A', 'de' as 'Trybe', 2 + 8 as 'eh';
+SELECT 5 + 6 AS 'A', 'de' AS 'Trybe', 2 + 8 AS 'eh';
