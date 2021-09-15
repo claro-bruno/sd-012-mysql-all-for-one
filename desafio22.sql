3aa8344ffcb08cf577e7246867c1021c4b53bd38
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-09-14 21:08:07 -0300

    Resolve desafios 20, 21 e 22

---
+++ desafio22.sql
@@ -0,0 +1,6 @@
+-- 22 - Atualize os dados de `discount` do `order_details` para 15. 
+-- (Não é necessário utilizar o SAFE UPDATE em sua query).
+
+UPDATE northwind.order_details 
+SET 
+    discount = 15;
