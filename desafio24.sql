fd118e3f09dc42916a807183dcd519c2fb603ec6
Samuel Constantino <samuelicapuidados@gmail.com>
2021-09-14 21:36:15 -0300

    implementa requisitos de manipulação de tabela

---
+++ desafio24.sql
@@ -0,0 +1,10 @@
+/*
+Atualize os dados da coluna discount da tabela order_details para 45, 
+onde o valor na coluna unit_price seja maior que 10.0000 e o id seja um número entre 30 e 40.
+*/
+
+UPDATE northwind.order_details 
+SET 
+    discount = 45
+WHERE
+    unit_price > 10.0000 AND (id BETWEEN 30 AND 40);
---
+++ desafio27.sql
@@ -0,0 +1,5 @@
+/*
+ Delete todos os dados da tabela order_details
+*/
+
+DELETE FROM northwind.order_details;
