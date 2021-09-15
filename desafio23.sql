f8a776693c3ce145618f53fd99a08aeb071d2aad
jorda <jordan.rachid@gmail.com>
2021-09-15 07:39:32 -0300

    finaliza desafios de update

--- desafio23.sql
+++ desafio23.sql
@@ -1,1 +1,1 @@
-UPDATE northwind.order_details SET discount = 30 WHERE unit_price = 10;
+UPDATE northwind.order_details SET discount = 30 WHERE unit_price < 10;
--- desafio24.sql
+++ desafio24.sql
@@ -1,1 +1,1 @@
-UPDATE northwind.order_details SET discount = 43 WHERE unit_price > 10 AND id BETWEEN 30 AND 40;
+UPDATE northwind.order_details SET discount = 45 WHERE unit_price > 10 AND id BETWEEN 30 AND 40;
