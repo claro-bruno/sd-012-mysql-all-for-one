35a77ea99d50132f97d8ff1f0aec80ffe31e1c54
Daniel Ceci <danielceci96@gmail.com>
2021-09-15 14:20:44 -0300

    requisito 12

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 SELECT notes FROM northwind.purchase_orders WHERE notes LIKE "%3%";
--- o % simboliza um monte de string. Eu queria botar: generated based on Order >= 30(ou <=)
+-- o % simboliza uma ou várias strings. Eu queria botar: generated based on Order >= 30(ou <=)
 -- Ficaria muito grande. Então posso colocar o % antes do 3 pra representar a frase: generated based on Order
--- depois do 3 poderia ser um _ também. Aí representaria que tem apenas 1 caracter depois do 3. Mas
+-- depois do 3 poderia ser um _ também. Aí representaria que tem apenas 1 caracter depois do 3(0 ou 9). Mas
 -- dá certo usando um % de novo.
