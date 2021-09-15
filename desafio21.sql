79d2f45cb804e2a158e63a2231075aadf9eeaefd
rafaelprimon <rafaelfernandesprimon@gmail.com>
2021-09-15 11:25:46 -0300

    desafios 14 a 24

--- desafio21.sql
+++ desafio21.sql
@@ -8,4 +8,5 @@ date_allocated,
 purchase_order_id,
 inventory_id)
 VALUES
-(69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129); 
+(69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129),
+(69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
