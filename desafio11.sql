e307fc5d7033aeca76a041d138faf8b54bd5357a
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-09-14 21:47:21 -0300

    feito todos os requisitos do filtering

---
+++ desafio11.sql
@@ -0,0 +1,6 @@
+SELECT 
+    notes
+FROM
+    purchase_orders
+WHERE
+    id BETWEEN 96 AND 100;
---
+++ desafio12.sql
@@ -0,0 +1,6 @@
+SELECT 
+    submitted_date
+FROM
+    purchase_orders
+WHERE
+    submitted_date BETWEEN '2006-04-26 00:00:00' AND '2006-04-26 23:59:59';
