507a2145af07aedf1f2d5e89cdfcabfd117e057c
Renata Nunes <renatanunes231@yahoo.com.br>
2021-09-14 23:11:10 -0300

    Formatando os Desafios de manipulacao de tabelas

--- desafio20.sql
+++ desafio20.sql
@@ -3,7 +3,5 @@
 
 -- obs: o id deve ser incrementado automaticamente.
 
-INSERT INTO
-	northwind.order_details (order_id, product_id, quantity, unit_price, status_id, inventory_id)
-VALUES
-	(69, 80, 15.0000, 15.0000, 2, 129);
+INSERT INTO northwind.order_details (order_id, product_id, quantity, unit_price, status_id, inventory_id)
+VALUES (69, 80, 15.0000, 15.0000, 2, 129);
--- desafio21.sql
+++ desafio21.sql
@@ -1,7 +1,4 @@
 -- Adicione com um único INSERT, duas linhas à tabela order_details com os mesmos dados do requisito 20.
 
-INSERT INTO
-	northwind.order_details (order_id, product_id, quantity, unit_price, status_id, inventory_id)
-VALUES
-	(69, 80, 15.0000, 15.0000, 2, 129),
-	(69, 80, 15.0000, 15.0000, 2, 129);
+INSERT INTO northwind.order_details (order_id, product_id, quantity, unit_price, status_id, inventory_id)
+VALUES  (69, 80, 15.0000, 15.0000, 2, 129), (69, 80, 15.0000, 15.0000, 2, 129);
