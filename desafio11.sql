56dda0be38d5cbd8becaba5e9947c9143d3913a5
Eric Kreis <erickreis793@gmail.com>
2021-09-14 21:40:09 -0300

    [ Code Climate ] - Resolvendo identaçao

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,6 @@
-SELECT
+SELECT 
   notes
 FROM
   northwind.purchase_orders
 WHERE
-	notes
-BETWEEN
-	'Purchase generated based on Order #30'
-  AND 'Purchase generated based on Order #39';
+  notes BETWEEN 'Purchase generated based on Order #30' AND 'Purchase generated based on Order #39';
