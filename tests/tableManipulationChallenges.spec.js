61a767099d098e0ee23c15bfcd54cf3cdaa1f415
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-09-14 22:39:33 -0300

    feito todos os requisitos tableManipuling

--- tests/tableManipulationChallenges.spec.js
+++ tests/tableManipulationChallenges.spec.js
@@ -26,7 +26,7 @@ describe('Desafios de manipulação de tabelas', () => {
 
   afterEach(async () => await sequelize.query('DROP DATABASE northwind;', { type: 'RAW' }));
 
-  describe.only('Queries de inserção', () => {
+  describe('Queries de inserção', () => {
     const countOrderDetailsQuery = `SELECT COUNT(*) AS details_count FROM northwind.order_details
       WHERE order_id = 69
             AND product_id = 80
