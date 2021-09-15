c3fd9ba4063d2a841e94c2bafe61b392e8355ec4
Renata Nunes <renatanunes231@yahoo.com.br>
2021-09-14 23:05:59 -0300

    Desafios de manipulacao de tabelas

---
+++ desafio26.sql
@@ -0,0 +1,5 @@
+-- Delete todos os dados em que a unit_price da tabela order_details seja maior que 10.0000.
+
+DELETE FROM northwind.order_details 
+WHERE
+    unit_price > 10.0000;
---
+++ desafio27.sql
@@ -0,0 +1,3 @@
+-- Delete todos os dados da tabela order_details.
+
+TRUNCATE northwind.order_details;
